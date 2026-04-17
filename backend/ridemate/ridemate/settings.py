9f8a890070064a9ae8ba07ee0843544551580881
Pratyansh08 <2022003935.pratyansh@ug.sharda.ac.in>
2026-04-17 10:35:53 +0530

    Allow Render hostname automatically

--- backend/ridemate/ridemate/settings.py
+++ backend/ridemate/ridemate/settings.py
@@ -43,6 +43,10 @@ if allowed_hosts_env:
 else:
     ALLOWED_HOSTS = []
 
+render_external_hostname = os.getenv("RENDER_EXTERNAL_HOSTNAME", "").strip()
+if render_external_hostname and render_external_hostname not in ALLOWED_HOSTS:
+    ALLOWED_HOSTS.append(render_external_hostname)
+
 
 # Application definition
 
